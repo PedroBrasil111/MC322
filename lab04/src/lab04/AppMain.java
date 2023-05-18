5cb47414b883932eb91df9bb257ab7cc52801c39
Pedro Brasil Barroso <p260637@dac.unicamp.br>
2023-05-18 17:16:10 -0300

    remover classe de Leitura

--- lab04/src/lab04/AppMain.java
+++ lab04/src/lab04/AppMain.java
@@ -12,99 +12,6 @@ public class AppMain {
 	/* Lista contendo todas as seguradoras instanciadas (base para utilizar os menus) */
 	private static List<Seguradora> listaSeguradoras = new ArrayList<Seguradora>();
 
-	/* Classe de leitura (para só instanciar um scanner) */
-	private static class Leitura {
-		private static Scanner scanner = new Scanner(System.in);
-		/* Lê até que seja dado um inteiro e o retorna.
-		 * Imprime mensagem de erro quando não é inteiro. */
-		public static int lerInteiro() {
-			String input;
-			do {
-				input = scanner.nextLine();
-				try {
-					return Integer.parseInt(input);
-				} catch (NumberFormatException e) { // não é inteiro
-					System.out.println("Erro. Digite um numero inteiro valido.");
-				}
-			} while (true);
-		}
-		/* Lê uma string e a retorna */
-		public static String lerString() {
-			return scanner.nextLine();
-		}
-		/* Lê até que seja dada uma data no formato dd/MM/aaaa e retorna o Date equivalente.
-		 * Imprime mensagem de erro quando data não é dada no formato */
-		public static Date lerData() {
-			String input;
-			Date data;
-			do {
-				input = Leitura.lerString();
-				if (Validacao.validaData(input)) {
-					data = Data.stringToDate(input);
-					break;
-				}
-				System.out.println("Erro. Data invalida ou fora do formato especificado. " +
-						"Tente digitar novamente.");
-			} while (true);
-			return data;
-		}
-		/* Dado o tamanho (tam) de uma lista ou array, retorna se i é um índice válido do iterável. */
-		private static boolean indiceValido(int i, int tam) {
-			if (i < 0 || i >= tam)
-				return false;
-			return true;
-		}
-		public static int lerIndice(int tam) {
-			int pos;
-			do {
-				pos = lerInteiro();
-				// pos >= 0 e pos < tam
-				if (indiceValido(pos, tam))
-					return pos;
-				System.out.println("Erro. Digite um valor valido.");
-			} while (true);
-		}
-		/* Lê até que um nome válido seja dado (nome sem números) e o retorna */
-		public static String lerNome() {
-			String nome;
-			do {
-				nome = lerString();
-				if (Validacao.validaNome(nome))
-					break;
-				System.out.println("Erro. Digite um nome valido.");
-			} while (true);
-			return nome;
-		}
-		/* Lê até que um CNPJ válido seja dado e o reteorna */
-		public static String lerCnpj() {
-			String cnpj;
-			do {
-				cnpj = lerString();
-				if (! Validacao.validarCNPJ(cnpj))
-					System.out.println("CNPJ invalido. Tente digitar novamente.");
-				else
-					break;
-			} while (true);
-			return cnpj;
-		}
-		/* Lê até que um CPF válido seja dado e o retorna */
-		public static String lerCpf() {
-			String cpf;
-			do {
-				cpf = Leitura.lerString();
-				if (! Validacao.validarCPF(cpf))
-					System.out.println("CPF invalido. Tente digitar novamente.");
-				else
-					break;
-			} while (true);
-			return cpf;
-		}
-		public static void fechar() {
-			scanner.close();
-		}
-
-	}
-
 	/* Métodos para os menus (adaptados de https://github.com/rebecapadovani/ExemploEnumMenu) */
 	//exibir menu externo
 	private static void exibirMenuExterno() {
@@ -118,8 +25,7 @@ public class AppMain {
 	 * se a lista de constantes do submenu for percorrida da mesma forma que o meu externo, a opcão Voltar
 	 * é printada com a posicão que está na lista do enum (9 - Voltar). Por isso, a lista é percorrida 
 	 * de forma diferente, tendo i como o inteiro correspondente. Assim, para listar o submenu de cadastros,
-	 * por exemplo, vai ser printado "3 - Voltar".
-	 */
+	 * por exemplo, vai ser printado "3 - Voltar". */
 	private static void exibirSubmenu(MenuOperacoes op) {
 		SubmenuOperacoes[] submenu = op.getSubmenu();
 		System.out.println("** " + op.getDescricao() + " **");
