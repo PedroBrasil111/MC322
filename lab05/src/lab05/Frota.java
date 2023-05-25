6b5495c74f13bc84121efde4210eb906c127366f
Pedro Brasil <p260637@dac.unicamp.br>
2023-05-24 22:44:52 -0300

    Add métodos

--- lab05/src/lab05/Frota.java
+++ lab05/src/lab05/Frota.java
@@ -1,5 +1,33 @@
 package lab05;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Frota {
+    private String code;
+    private List<Veiculo> listaVeiculos = new ArrayList<Veiculo>();
+
+    public boolean addVeiculo(String placa, String marca, String modelo, int anoFabricacao) {
+        return listaVeiculos.add(new Veiculo(placa, marca, modelo, anoFabricacao));
+    }
+
+    public boolean removeVeiculo(String placa) {
+        for (int i = 0; i < listaVeiculos.size(); i++)
+            if (listaVeiculos.get(i).getPlaca() == placa)
+                return listaVeiculos.remove(listaVeiculos.get(i));
+        return false;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public List<Veiculo> getListaVeiculos() {
+        return listaVeiculos;
+    }
 
 }
