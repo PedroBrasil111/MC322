18ec89d306582fdcb9b03ac0ceee0d29c65c74e1
Pedro Brasil <p260637@dac.unicamp.br>
2023-06-04 13:17:37 -0300

    teste

--- lab05/src/lab05/AppMain.java
+++ lab05/src/lab05/AppMain.java
@@ -3,8 +3,24 @@ package lab05;
 public class AppMain {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Seguradora s = new Seguradora("1", "A", "1", "B", "C");
+		double valor = 10. * (1.25) * (1 + 1./(1 + 2)) * (2 + 3/10.) * (5 + 4/10.);
+		// 207
+		System.out.println(valor);
+		/*
+		public double calcularValor() {
+        	double qtdeSinistrosPorCondutor = 0.;
+        	double valor = CalcSeguro.VALOR_BASE.getValor() *
+        	    // FATOR_IDADE -- 
+        	    CalcSeguro.values()[Data.calcularIdade(cliente.getDataNasc())/30 + 1].getValor() *
+        	    (1 + 1/(cliente.getListaVeiculos().size() + 2)) *
+        	    (2 + getSeguradora().getSinistrosPorCliente(cliente).size()/10);
+        	for (Condutor condutor: getListaCondutores())
+        	    qtdeSinistrosPorCondutor += condutor.getListaSinistros().size();
+        	valor *= 5 + qtdeSinistrosPorCondutor/10;
+        	return valor;
+    	}
+		*/
 	}
 
 }
